d367425f373a3378908b70ead3f20d9ef8b213fc
Javier Martin <martin9sek@gmail.com>
2016-08-19 22:42:21 +0200

    Added missing web server app initialization

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ var express = require("express");
 
 var app = express();
 app.use(express.static(__dirname + "/dist"));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
