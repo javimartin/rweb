dcef24f35ff480732fd8cd066ed123e408b4b747
Javier Martin <martin9sek@gmail.com>
2016-11-12 15:43:20 +0100

    - Added navigation toggle for small screens (iPhone, etc..) - Changed behavior of the R big logo to scroll with the page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,5 @@
 'use strict';
 
-var scroll_start = 0;
-var startchange = $('.main-content');
-var offset = startchange.offset();
-if (startchange.length) {
-    $(document).scroll(function () {
-        scroll_start = $(this).scrollTop();
-        if (scroll_start > offset.top) {
-            $(".logo-r").css('opacity', '0');
-        } else {
-            $('.logo-r').css('opacity', '1');
-        }
-    });
-}
-
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngSanitize',
